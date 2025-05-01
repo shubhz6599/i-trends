f7bb97c797175457057893698de6b151d53db0ef
Shubham Karkhile <shubhz6599@gmail.com>
2025-05-01 15:50:38 +0530

    NavBar BugFix

--- src/app/components/top-navbar/top-navbar.component.ts
+++ src/app/components/top-navbar/top-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,36 +11,49 @@ export class TopNavbarComponent implements OnInit {
   isMenuOpen = false;
   cartCount = 0; // You can dynamically update it later from your cart service
   searchQuery: string = '';
-  constructor(private authService: AuthService, private router: Router) { }
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const clickedInsideMenu = target.closest('.nav-content') || target.closest('.hamburger');
 
-  ngOnInit(): void {
-    this.fetchCartItems()
+    if (!clickedInsideMenu && this.isMenuOpen) {
+      this.closeMenu();
+    }
   }
-  fetchCartItems(): void {
-    this.authService.getCart().subscribe(
-      (response) => {
-        if (response.cart.items.length > 0) {
-          this.cartCount = response.cart.items.length;
-        } else {
-          this.cartCount = 0;
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+    constructor(private authService: AuthService, private router: Router) { }
+
+    ngOnInit(): void {
+      this.fetchCartItems()
+    }
+    fetchCartItems(): void {
+      this.authService.getCart().subscribe(
+        (response) => {
+          if (response.cart.items.length > 0) {
+            this.cartCount = response.cart.items.length;
+          } else {
+            this.cartCount = 0;
+          }
+        },
+        (error) => {
+          console.error('Error fetching cart items:', error);
         }
-      },
-      (error) => {
-        console.error('Error fetching cart items:', error);
-      }
-    );
-  }
+      );
+    }
 
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
 
-  onSearch(): void {
-    if (this.searchQuery.trim()) {
+    onSearch(): void {
+      if(this.searchQuery.trim()) {
       this.router.navigate([`category/all-products/${this.searchQuery}`], {
         queryParams: { query: this.searchQuery },
       });
+      this.toggleMenu()
     }
   }
 }
