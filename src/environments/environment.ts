55928b2eda3614360ee2dea9feab4a5f17be51b0
Shubham Karkhile <shubhz6599@gmail.com>
2025-06-05 20:45:57 +0530

    jh

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment: any = {
-  "apiBaseUrl": "https://i-trends-backend-production.up.railway.app/api/auth",
-  "userbaseUrl":"https://i-trends-backend-production.up.railway.app/api/user",
+  "apiBaseUrl": "https://api.itrendss.com/api/auth",
+  "userbaseUrl":"https://api.itrendss.com/api/user",
   "razorPayKey": "rzp_live_UL7ccDjcSlGzt2",
   // "apiBaseUrl": "http://localhost:5000/api/auth",
   // "userbaseUrl": "http://localhost:5000/api/user"
