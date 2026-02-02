f0429002a24d2becb46a8206fed04d0692f4e9a4
Shubham Karkhile <shubhz6599@gmail.com>
2026-02-02 09:48:03 +0530

    APi Render URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment: any = {
-  "apiBaseUrl": "https://api.itrendss.com/api/auth",
-  "userbaseUrl":"https://api.itrendss.com/api/user",
+  "apiBaseUrl": "https://i-trends-backend.onrender.com/api/auth",
+  "userbaseUrl":"https://i-trends-backend.onrender.com/api/user",
   "razorPayKey": "rzp_live_UL7ccDjcSlGzt2",
   // "apiBaseUrl": "http://localhost:5000/api/auth",
   // "userbaseUrl": "http://localhost:5000/api/user"
